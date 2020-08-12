ff1f6cae1bb309aa2c58bcc1b9d5100bdaccd6de
Hari Subedi <princethelittleking@gmail.com>
2020-08-12 23:23:23 +0545

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return (
+        <div className='ui container' style={{marginTop:'24px'}}>
+            <SearchBar />
+        </div>
+    );
+};
+
+export default App;
