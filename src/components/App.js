786d544dc868c8182c146add247d4fc4486e429f
Hari Subedi <princethelittleking@gmail.com>
2020-08-14 21:48:30 +0545

    invokes callback on children

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import { render } from '@testing-library/react';
 
-const App = () => {
-    return (
-        <div className='ui container' style={{marginTop:'24px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+
+    }
+    render() {
+        return (
+            <div className='ui container' style={{ marginTop: '24px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+}
 
 export default App;
